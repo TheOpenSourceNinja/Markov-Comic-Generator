c9b67e4af06b741e10dd1a76bf8285df86ff0d91
James Dearing <dearingj@lifetime.oregonstate.edu>
2015-07-30 22:10:17 -0700

    Lot of stuff. Biggest change is probably the addition of docstrings. Added --help command line option. Added verification of command line options (inDir exists and is a directory, output files are not directories, etc.). Made the program obey the number of comics parameter. Font size now estimated base on image height. Fixed header/filename verification in generator.py.

--- generator.py
+++ generator.py
@@ -2,31 +2,56 @@
 
 import random
 import os
+import sys
 
 class Generator:
 	class MarkovNode:
 		def __init__( self, isEnd ):
+			'''Initialize. Duh.
+				Args:
+					isEnd: A Boolean indicating whether this node is the end of a sentence.
+			'''
 			self.links = dict()
 			self.isEndOfSentence = isEnd
 	
 		def getRandomLinkedWord( self ):
+			'''Randomly select one of the nodes to which this node is linked.
+			'''
 			return random.choice( list( self.links.keys() ) ) #TODO: Use weighted randomness
 	
 		def hasLinks( self ):
+			'''Determine whether this node has any links.
+			'''
 			return len( self.links ) > 0
 	
 		def addLink( self, other ):
+			'''Add to this node a link to another node.
+				Args:
+					other: The other node to which to link.
+				Returns:
+					The number of links this node has after adding the new link.
+			'''
 			if other not in self.links:
 				self.links[ other ] = 1
 			else:
-				self.links[ other ] += 1
+				self.links[ other ] += 1 #If there is already a link to the other node, increase the link's value.
 			return len( self.links )
 	
-	def __init__( self, charLabel ):
+	def __init__( self, charLabel, cm = "//" ):
+		'''Just does what the name implies.
+			Args:
+				charLabel: A string naming which comic character this generator represents.
+				cm: A string used to mark the beginning of comments. Including it here ensures that this object will use the same kind of comment marker as the rest of the program. Defaults to "//".
+		'''
 		self.charLabel = charLabel.upper()
 		self.lines = []
+		self.commentMark = cm
 	
-	def buildDatabase( self, inDir ):
+	def buildGraph( self, inDir ):
+		'''Build the Markov graph for this generator's comic character.
+			Args:
+				inDir: The directory in which to find the 'transcripts' subdirectory. The 'transcripts' subdirectory is where we will actually look for everything.
+		'''
 		transcriptDir = os.path.join( inDir, "transcripts" )
 		for inFileName in os.listdir( transcriptDir ):
 			inFileName = os.path.join( transcriptDir, inFileName )
@@ -36,16 +61,19 @@ class Generator:
 			headerFound = False
 		
 			while keepLookingForHeader:
-				line = inFile.readline()
-				line = line.partition( "//" )[0].strip()
-				if line.isnumeric() and line == os.path.basename( inFileName ):
+				line = inFile.readline().partition( self.commentMark )[0].strip()
+				if line.isnumeric() and line == os.path.splitext( os.path.basename( inFileName ) )[0]: #Why enforce the file name requirement? It's about people, not code: it's to make sure transcribers have read all available documentation (README.md) about the transcription format.
 					keepLookingForHeader = False
 					headerFound = True
 				elif len(line) > 0:
 					keepLookingForHeader = False
+			
+			if not headerFound:
+				print( "Error: File", inFileName, "is not a properly formatted transcript.", file=sys.stderr )
+				break;
 		
 			for line in inFile:
-				line = line.partition( "//" )[0].strip()
+				line = line.partition( self.commentMark )[0].strip()
 				if( len( line ) > 0 ):
 					line = line.split()
 					speaker = line[0].rstrip(":").strip()
@@ -79,8 +107,14 @@ class Generator:
 		
 							previousWord = word
 
-	def generate(self, numberOfSentences):
-		result = ""
+	def generateSentences(self, numberOfSentences = 1):
+		'''Generate some number of sentences (paths through the Markov graph).
+			Args:
+				numberOfSentences: The number of sentences to generate. Defaults to 1.
+			Returns:
+				A list of strings, each string containing one sentence.
+		'''
+		result = []
 		for i in range ( numberOfSentences ):
 			currentWord = random.choice( self.sentenceStarts )
 			sentence = currentWord
@@ -88,7 +122,5 @@ class Generator:
 				currentWord = self.nodes[ currentWord ].getRandomLinkedWord()
 				sentence += " " + currentWord
 			
-			result += sentence + " "
+			result.append( sentence )
 		return result
-
-
--- main.py
+++ main.py
@@ -18,14 +18,24 @@ EX_NOINPUT = 66 #Input not openable
 EX_CANTCREAT = 73 #Can't create output file
 EX_NOPERM = 77 #Permission error
 
+#Set defaults
 silence = silenceDefault = False
 inDir = inDirDefault = "./"
 outTextFileName = outTextFileNameDefault = "default out.txt"
 outImageFileName = outImageFileNameDefault = "default out.png"
 numberOfComics = numberOfComicsDefault = 1
 saveForWeb = saveForWebDefault = False
+commentMark = commentMarkDefault = "//" #If in the future we decide to use a different mark for comments, this is the only line we'll need to change.
 
 def findCharsPerLine( text, font, maxWidth ):
+	'''Find how many characters will fit within the specified width.
+		Args:
+			text: The string whose contents are used to test character width.
+			font: The font used to test character width.
+			maxWidth: The maximum width in pixels.
+		Returns:
+			An integer indicating how many characters fit within maxWidth.
+	'''
 	charsPerLine = maxWidth // font.getsize( "L" )[0]
 	
 	while font.getsize( text[ :charsPerLine ] )[0] > maxWidth:
@@ -34,6 +44,14 @@ def findCharsPerLine( text, font, maxWidth ):
 	return charsPerLine
 
 def rewrap( text, font, maxWidth ):
+	'''Rewrap and center text.
+		Args:
+			text: A string containing the text to be wrapped.
+			font: Gets passed to findCharsPerLine().
+			maxWidth: Gets passed to findCharsPerLine().
+		Returns:
+			A list of strings.
+	'''
 	charsPerLine = findCharsPerLine( text, font, maxWidth )
 	temp = textwrap.wrap( text, width = charsPerLine )
 	
@@ -45,6 +63,8 @@ def rewrap( text, font, maxWidth ):
 	return result
 
 def usage():
+	'''Print command line usage info.
+	'''
 	print( "😕" ) #In case of transcoding errors: this should be U+1F615, "confused face"
 	print( "Usage: The program takes the following command line arguments:" )
 	print( "🞍 -s or --silent: Prevents output on standard out. Defaults to", silenceDefault ) #the first character of each of these should be U+1F78D, "black slightly small square"
@@ -53,9 +73,10 @@ def usage():
 	print( "🞍 -p or --outimagefile: The name of an image file to save the resulting comic to. Numbers will be appended if multiple comics are generated. Defaults to", outImageFileNameDefault )
 	print( "🞍 -n or --number: The number of comics to generate. Defaults to", numberOfComicsDefault )
 	print( "🞍 -w or --saveforweb: If specified, saves the images using settings which result in a smaller file size, possibly at the expense of image quality." )
+	print( "🞍 -h or --help: Display this usage info." )
 
 try:
-	options, argsLeft = getopt.getopt( sys.argv[1:], "swi:o:p:n:", ["silent", "saveforweb", "indir=", "outtextfile=", "outimagefile=", "number="] )
+	options, argsLeft = getopt.getopt( sys.argv[1:], "swhi:o:p:n:", ["silent", "saveforweb", "help", "indir=", "outtextfile=", "outimagefile=", "number="] )
 except getopt.GetoptError as error:
 	print( error )
 	usage()
@@ -74,145 +95,183 @@ for option in options:
 		numberOfComics = int( option[1].strip( "=" ) )
 	elif option[0] == "-w" or option[0] == "--saveforweb":
 		saveForWeb = True
+	elif option[0] == "-h" or option[0] == "--help":
+		usage()
+		sys.exit( EX_OK )
+
+#Verify user input
+if not os.path.isdir( inDir ):
+	print( "Error:", inDir, "is not a directory.", file=sys.stderr )
+	exit( EX_NOINPUT )
+if os.path.exists( outTextFileName ) and not os.path.isfile( outTextFileName ):
+	print( "Error:", outTextFileName, "is not a file.", file=sys.stderr )
+	exit( EX_CANTCREAT )
+if os.path.exists( outImageFileName ) and not os.path.isfile( outImageFileName ):
+	print( "Error:", outImageFileName, "is not a file.", file=sys.stderr )
+	exit( EX_CANTCREAT )
+if numberOfComics < 1:
+	print( "Error: Number of comics (", numberOfComics, ") is less than 1.", file=sys.stderr )
+	exit( EX_USAGE )
 
 if not silence:
 	print( "Copyright 2015 James Dearing. Licensed under the GNU Affero General Public License (AGPL), either version 3.0 or (at your option) any later version published by the Free Software Foundation. You should have received a copy of the AGPL with this program. If you did not, you can find version 3 at https://www.gnu.org/licenses/agpl-3.0.html or the latest version at https://www.gnu.org/licenses/agpl.html" )
 
 wordBubblesDir = os.path.join( inDir, "word-bubbles" )
 
-try:
-	wordBubbleFileName = random.choice( os.listdir( wordBubblesDir ) )
-except IndexError as error:
-	print( error, file=sys.stderr )
-	exit( EX_NOINPUT )
+for generatedComicNumber in range( numberOfComics ):
 
-comicID = os.path.splitext( wordBubbleFileName )[0]
-wordBubbleFileName = os.path.join( wordBubblesDir, wordBubbleFileName )
-if not silence:
-	print( wordBubbleFileName )
+	try:
+		wordBubbleFileName = random.choice( os.listdir( wordBubblesDir ) )
+	except IndexError as error:
+		print( error, file=sys.stderr )
+		exit( EX_NOINPUT )
 
-try:
-	wordBubbleFile = open( file=wordBubbleFileName, mode="rt" )
-except OSError as erro:
-	print( error, file=sys.stderr )
-	exit( EX_NOINPUT )
+	comicID = os.path.splitext( wordBubbleFileName )[0]
+	wordBubbleFileName = os.path.join( wordBubblesDir, wordBubbleFileName )
+	if not silence:
+		print( "Loading word bubbles from", wordBubbleFileName )
 
-lookForSpeakers = True
-while lookForSpeakers:
-	line = wordBubbleFile.readline()
-	line = line.partition( "//" )[0].strip()
-	if len( line ) > 0:
-		speakers = line.split( "\t" )
-		if len( speakers ) > 0:
-			lookForSpeakers = False
+	try:
+		wordBubbleFile = open( file=wordBubbleFileName, mode="rt" )
+	except OSError as erro:
+		print( error, file=sys.stderr )
+		exit( EX_NOINPUT )
 
-if not silence:
-	print( speakers )
+	lookForSpeakers = True
+	while lookForSpeakers:
+		line = wordBubbleFile.readline()
+		line = line.partition( commentMark )[0].strip()
+		if len( line ) > 0:
+			speakers = line.split( "\t" )
+			if len( speakers ) > 0:
+				lookForSpeakers = False
 
-generators = dict()
-for speaker in speakers:
-	newGenerator = Generator( charLabel = speaker )
-	newGenerator.buildDatabase( inDir )
-	generators[ speaker ] = newGenerator
+	if not silence:
+		print( "These characters speak:", speakers )
 
-if not silence:
-	print( comicID )
+	generators = dict()
+	for speaker in speakers:
+		newGenerator = Generator( charLabel = speaker, cm = commentMark )
+		newGenerator.buildGraph( inDir )
+		generators[ speaker ] = newGenerator
 
-imageDir = os.path.join( inDir, "images" )
-inImageFileName = os.path.join( imageDir, comicID + ".png" )
+	if not silence:
+		print( comicID )
 
-try:
-	image = Image.open( inImageFileName ).convert() #Text rendering looks better if we ensure the image's mode is not palette-based. Calling convert() with no mode argument does this.
-except IOError as error:
-	print( error, file=sys.stderr )
-	exit( EX_NOINPUT )
+	imageDir = os.path.join( inDir, "images" )
+	inImageFileName = os.path.join( imageDir, comicID + ".png" )
 
-normalFontSize = 30
-try:
-	font = ImageFont.truetype( "Nina fonts/NinaMedium.ttf", size=normalFontSize )
-except IOError:
-	print( error, "\nUsing default font instead.", file=sys.stderr )
-	font = ImageFont.load_default()
+	try:
+		image = Image.open( inImageFileName ).convert() #Text rendering looks better if we ensure the image's mode is not palette-based. Calling convert() with no mode argument does this.
+	except IOError as error:
+		print( error, file=sys.stderr )
+		exit( EX_NOINPUT )
 
-try:
-	outFile = open( file=outTextFileName, mode="at" )
-except OSError as error:
-	print( error, "\nUsing standard output instead", file=sys.stderr )
-	outFile = sys.stdout
-
-for line in wordBubbleFile:
-	line = line.partition( "//" )[0].strip()
-	if len( line ) > 0:
-		line = line.split( "\t" )
-		character = line[0]
-		character = character.rstrip( ":" ).strip()
+	initialFontSize = image.size[1] // 2 #Contrary to the claim by PIL's documentation, font sizes are apparently in pixels, not points. The size being requested is the height of a generic character; the actual height of any particular character will be approximately (not exactly) the requested size. Assume here that we want one line of text to fill half the height of the image; we will try smaller and smaller sizes later.
+	try:
+		font = ImageFont.truetype( "Nina fonts/NinaMedium.ttf", size=initialFontSize )
+	except IOError: #TODO: Don't immediately use the default font. If the font file in ./ doesn't work, use FreeType to find something similar.
+		print( error, "\nUsing default font instead.", file=sys.stderr )
+		font = ImageFont.load_default()
+	
+	if numberOfComics > 1:
+		oldOutTextFileName = outTextFileName
+		temp = os.path.splitext( outTextFileName )
+		outTextFileName = temp[0] + str( generatedComicNumber ) + temp[1]
+	
+	try:
+		outFile = open( file=outTextFileName, mode="wt" )
+	except OSError as error:
+		print( error, "\nUsing standard output instead", file=sys.stderr )
+		outFile = sys.stdout
+	
+	if numberOfComics > 1:
+		outTextFileName = oldOutTextFileName
+	
+	print( comicID, file=outFile )
+	
+	for line in wordBubbleFile:
+		line = line.partition( commentMark )[0].strip()
+		if len( line ) > 0:
+			line = line.split( "\t" )
+			character = line[0]
+			character = character.rstrip( ":" ).strip()
 		
-		generator = generators[ character ]
-		text = generator.generate( 1 )
-		print( character, ": ", text, sep="", file=outFile )
-		if not silence:
-			print( character, ": ", text, sep="" )
+			generator = generators[ character ]
+			text = " ".join( generator.generateSentences( 1 ) )
+			print( character, ": ", text, sep="", file=outFile )
+			if not silence:
+				print( character, ": ", text, sep="" )
 		
-		topLeftX = int( line[1] )
-		topLeftY = int( line[2] )
-		bottomRightX = int( line[3] )
-		bottomRightY = int( line[4] )
+			topLeftX = int( line[1] )
+			topLeftY = int( line[2] )
+			bottomRightX = int( line[3] )
+			bottomRightY = int( line[4] )
 		
-		box = ( topLeftX, topLeftY, bottomRightX, bottomRightY )
-		wordBubble = image.crop( box )
-		draw = ImageDraw.Draw( wordBubble )
+			box = ( topLeftX, topLeftY, bottomRightX, bottomRightY )
+			wordBubble = image.crop( box )
+			draw = ImageDraw.Draw( wordBubble )
 		
-		newText = rewrap( text, font, bottomRightX - topLeftX )
+			newText = rewrap( text, font, bottomRightX - topLeftX )
 		
-		margin = 0
-		offset = originalOffset = 0
-		fontSize = normalFontSize
-		goodSizeFound = False
-		usedFont = font
-		while not goodSizeFound:
+			margin = 0
+			offset = originalOffset = 0
+			fontSize = initialFontSize
+			goodSizeFound = False
+			usedFont = font
+			while not goodSizeFound:
+				offset = originalOffset
+				for line in newText:
+					offset += usedFont.getsize( line )[1]
+				if offset > bottomRightY - topLeftY:
+					fontSize -= 1
+					try:
+						usedFont = ImageFont.truetype( "Nina fonts/NinaMedium.ttf", size=fontSize )
+					except IOError as error:
+						print( error, "\nUsing default font instead.", file=sys.stderr )
+						usedFont = ImageFont.load_default()
+					newText = rewrap( text, usedFont, bottomRightX - topLeftX )
+				else:
+					goodSizeFound = True
+		
+			midX = int( wordBubble.size[0] / 2 )
+			midY = int( wordBubble.size[1] / 2 )
+		
+			try: #Choose a text color that will be visible against the background
+				backgroundColor = wordBubble.getpixel( ( midX, midY ) )
+				textColor = ( 255-backgroundColor[0], 255-backgroundColor[1], 255-backgroundColor[2], 255 )
+			except ValueError:
+				textColor = "black"
+			
 			offset = originalOffset
 			for line in newText:
+				draw.text( ( margin, offset ), line, font=usedFont, fill=textColor )
 				offset += usedFont.getsize( line )[1]
-			if offset > bottomRightY - topLeftY:
-				fontSize -= 1
-				try:
-					usedFont = ImageFont.truetype( "Nina fonts/NinaMedium.ttf", size=fontSize )
-				except IOError as error:
-					print( error, "\nUsing default font instead.", file=sys.stderr )
-					usedFont = ImageFont.load_default()
-				newText = rewrap( text, usedFont, bottomRightX - topLeftX )
-			else:
-				goodSizeFound = True
+				if offset > bottomRightY - topLeftY and not silence:
+					print( "Warning: Text is too big vertically.", file=sys.stderr )
 		
-		midX = int( wordBubble.size[0] / 2 )
-		midY = int( wordBubble.size[1] / 2 )
+			image.paste( wordBubble, box )
 		
-		try: #Choose a text color that will be visible against the background
-			backgroundColor = wordBubble.getpixel( ( midX, midY ) )
-			textColor = ( 255-backgroundColor[0], 255-backgroundColor[1], 255-backgroundColor[2], 255 )
-		except ValueError:
-			textColor = "black"
-			
-		offset = originalOffset
-		for line in newText:
-			draw.text( ( margin, offset ), line, font=usedFont, fill=textColor )
-			offset += usedFont.getsize( line )[1]
-			if offset > bottomRightY - topLeftY and not silence:
-				print( "Warning: Text is too big vertically.", file=sys.stderr )
-		
-		image.paste( wordBubble, box )
-		
-wordBubbleFile.close()
-outFile.close()
-
-try:
-	if saveForWeb:
-		image = image.convert( mode = "P", palette="WEB" )
-		image.save( outImageFileName, format="PNG", optimize=True )
-	else:
-		image.save( outImageFileName, format="PNG" )
-except IOError as error:
-	print( error, file=sys.stderr )
-	exit( EX_CANTCREAT )
+	wordBubbleFile.close()
+	outFile.close()
+	
+	if numberOfComics > 1:
+		oldOutImageFileName = outImageFileName
+		temp = os.path.splitext( outImageFileName )
+		outImageFileName = temp[0] + str( generatedComicNumber ) + temp[1]
+	
+	try:
+		if saveForWeb:
+			image = image.convert( mode = "P", palette="WEB" )
+			image.save( outImageFileName, format="PNG", optimize=True )
+		else:
+			image.save( outImageFileName, format="PNG" )
+	except IOError as error:
+		print( error, file=sys.stderr )
+		exit( EX_CANTCREAT )
+	
+	if numberOfComics > 1:
+		outImageFileName = oldOutImageFileName
+#end of loop: for generatedComicNumber in range( numberOfComics ):
 
 exit( EX_OK )
